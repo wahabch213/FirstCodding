74cff73b8b3bed168aaeb39e3710d062121ab5a3
Wahab Ch <ch213wahab@gmail.com>
2024-03-26 18:09:55 +0500

    Update Grid System/inclass2.css: change width, height, gap, grid-template-rows, grid-template-columns, place-self, and add grid-column and grid-row properties. Update .item2 and .item6 to have the same grid-column value.

---
+++ JS FirstClass/work.js
@@ -0,0 +1,38 @@
+
+let a = 5;
+let a1 = 1;
+let a2 = 2; 
+let a3 = 3;
+let a4 = 4;
+let a5 = 5;
+let a6 = 6;
+let a7 = 7;
+let a8 = 8;
+let a9 = 9;
+let a10= 10;
+// let b = 
+// let c = 
+
+const b = parseInt(prompt ("Enter Your Number For (+)"))
+const c = parseInt(prompt ("Enter Second Value"))
+let sum = b+c
+
+// document.write(sum)
+// const b1 = parseInt(prompt ("Enter Your Number For (-)"))
+// const c1 = parseInt(prompt ("Enter Second Value"))
+// let subtract = b1-c1
+// document.write(subtract)
+const b2 = parseInt(prompt ("Enter Your Number For (×)"))
+// const c2 = parseInt(prompt ("Enter Second Value"))
+let  multiply = b2*sum
+document.write(multiply)
+// const b3 = parseInt(prompt ("Enter Your Number For (÷)"))
+// const c3 = parseInt(prompt ("Enter Second Value"))
+// let  divide = b3/c3
+// document.write(divide)
+
+
+// document.write(bint+cint)
+// document.write(b+c)
+
+// console.log(a*a1,a*a2,a*a3,a*a4,a*a5,a*a6,a*a7,a*a8,a*a9,a*a10)
